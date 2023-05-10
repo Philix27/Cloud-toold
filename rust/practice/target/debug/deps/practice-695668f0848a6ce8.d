6c1caf80a82cbf79658a1b63278a3e3bf8ddb4f3
Philix27 <philixbob@gmail.com>
2023-05-10 23:53:42 +0100

    init rust

---
+++ rust/practice/target/debug/deps/practice-695668f0848a6ce8.d
@@ -0,0 +1,5 @@
+/Users/MAC/Documents/Devops/devops_practice/rust/practice/target/debug/deps/practice-695668f0848a6ce8: src/main.rs
+
+/Users/MAC/Documents/Devops/devops_practice/rust/practice/target/debug/deps/practice-695668f0848a6ce8.d: src/main.rs
+
+src/main.rs:
---
+++ rust/practice/target/debug/practice.d
@@ -0,0 +1,1 @@
+/Users/MAC/Documents/Devops/devops_practice/rust/practice/target/debug/practice: /Users/MAC/Documents/Devops/devops_practice/rust/practice/src/main.rs
